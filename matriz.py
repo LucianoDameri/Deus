552bc78e6ed6852c0d9da81893b93779dccbc4b4
LucianoDameri <lu.castelli@duocuc.cl>
2024-06-07 15:05:35 -0400

    first commit

---
+++ matriz.py
@@ -0,0 +1,13 @@
+matrix=[]
+
+
+for x in range(3):
+    persona=[]
+    nombre=input("ingrese el nombre: ")
+    apellido=input("Ingrese apellido: ")
+    persona.append(nombre)
+    persona.append(apellido)
+    matrix.append(persona)
+
+
+print(matrix)
